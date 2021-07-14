eed7e788c1224b63b71b1a8e0798ce850caeea28
nsridatta <nsridatta@gmail.com>
2021-07-14 15:22:17 +0530

    commit

---
+++ Server/src/test/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.springboot2jpacrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2JpaCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
