abc99677ce187277c0baf5c3f7e49cdebb279c43
nsridatta <nsridatta@gmail.com>
2021-07-20 17:28:29 +0530

    Modified Main class

--- Server/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplication.java
+++ Server/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplication.java
@@ -5,29 +5,40 @@ import net.guides.springboot2.springboot2jpacrudexample.repository.EmployeeRepos
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.*;
 import org.springframework.boot.actuate.env.EnvironmentEndpoint;
 import javax.annotation.PostConstruct;
+import java.util.Collections;
 
 @SpringBootApplication
+		(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+@ComponentScan(basePackages = "net.*")
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class Springboot2JpaCrudExampleApplication {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	private Employee employee;
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot2JpaCrudExampleApplication.class, args);
+
+		/*SpringApplication.run(Springboot2JpaCrudExampleApplication.class, args);*/
+		SpringApplication app = new SpringApplication(Springboot2JpaCrudExampleApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port","8080"));
+		app.run(args);
 	}
 
-	@Bean
-	InitializingBean sendDatabase() {
+	@Bean()
+	 public InitializingBean sendDatabase() {
 		return () -> {
-			employeeRepository.save(new Employee("John","Papa","John.papa@gmail.com"));
-			employeeRepository.save(new Employee("Rambo","Lars","rambo.lars@gmail.com"));
-			employeeRepository.save(new Employee("bamboo","tree","bamboo.tree@gmail.com"));
+			employeeRepository.save(new Employee("John","Papa","John.papa@gmail.com",  "2012-04-04"));
+			employeeRepository.save(new Employee("Rambo","Lars","rambo.lars@gmail.com","2014-06-06"));
+			employeeRepository.save(new Employee("bamboo","tree","bamboo.tree@gmail.com","2021-01-01"));
 		};
 	}
+
 	/*
 	To Find the list of beans present in the running spring boot application
 	*
