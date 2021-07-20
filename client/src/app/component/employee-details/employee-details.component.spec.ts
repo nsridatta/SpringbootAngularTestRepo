b1fa30b416d71c14806675022d66540ef47ca2bc
nsridatta <nsridatta@gmail.com>
2021-07-20 17:50:29 +0530

    Update employee-details.component.spec.ts

--- client/src/app/component/employee-details/employee-details.component.spec.ts
+++ client/src/app/component/employee-details/employee-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EmployeeDetailsComponent } from './employee-details.component';
 
@@ -6,7 +6,7 @@ describe('EmployeeDetailsComponent', () => {
   let component: EmployeeDetailsComponent;
   let fixture: ComponentFixture<EmployeeDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EmployeeDetailsComponent ]
     })
