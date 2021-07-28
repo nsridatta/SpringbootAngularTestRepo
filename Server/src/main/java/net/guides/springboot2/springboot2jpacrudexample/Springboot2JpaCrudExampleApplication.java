4b1de91ebde6b05c6d3e399de222cd34b4450d1c
nsridatta <nsridatta@gmail.com>
2021-07-28 21:40:00 +0530

    Modified code to enable caching and disable init data bean

--- Server/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplication.java
+++ Server/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplication.java
@@ -1,5 +1,7 @@
 package net.guides.springboot2.springboot2jpacrudexample;
 
+import net.guides.springboot2.springboot2jpacrudexample.exception.GlobalExceptionHandler;
+import net.guides.springboot2.springboot2jpacrudexample.exception.ResourceNotFoundException;
 import net.guides.springboot2.springboot2jpacrudexample.model.Employee;
 import net.guides.springboot2.springboot2jpacrudexample.repository.EmployeeRepository;
 import org.springframework.beans.factory.InitializingBean;
@@ -8,13 +10,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.*;
 import org.springframework.boot.actuate.env.EnvironmentEndpoint;
 import javax.annotation.PostConstruct;
 import java.util.Collections;
 
+@EnableCaching
 @SpringBootApplication
-		(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+		(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @ComponentScan(basePackages = "net.*")
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 public class Springboot2JpaCrudExampleApplication {
@@ -30,14 +34,15 @@ public class Springboot2JpaCrudExampleApplication {
 		app.run(args);
 	}
 
-	@Bean()
+	/*@Bean()
 	 public InitializingBean sendDatabase() {
 		return () -> {
-			employeeRepository.save(new Employee("John","Papa","John.papa@gmail.com",  "2012-04-04"));
-			employeeRepository.save(new Employee("Rambo","Lars","rambo.lars@gmail.com","2014-06-06"));
-			employeeRepository.save(new Employee("bamboo","tree","bamboo.tree@gmail.com","2021-01-01"));
+			employeeRepository.save(new Employee("John","Papa","John.papa@gmail.com","2012-04-04","India","Karnataka","Bangalore"));
+			employeeRepository.save(new Employee("Rambo","Lars","rambo.lars@gmail.com","2014-06-06","India","Telangana","Warangal"));
+			employeeRepository.save(new Employee("bamboo","tree","bamboo.tree@gmail.com","2021-01-01","India","Telangana","Hyderabad"));
+			employeeRepository.save(new Employee("bob","marley","bob.marley@gmail.com","2020-12-31","India","Telangana","Khammam"));
 		};
-	}
+	}*/
 
 	/*
 	To Find the list of beans present in the running spring boot application
