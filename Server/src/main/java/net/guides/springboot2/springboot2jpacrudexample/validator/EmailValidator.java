f1425099c9dd59e9b923b02b5b4b3d1943c4b8a8
nsridatta <nsridatta@gmail.com>
2021-07-28 21:38:23 +0530

    Modified

--- Server/src/main/java/net/guides/springboot2/springboot2jpacrudexample/validator/EmailValidator.java
+++ Server/src/main/java/net/guides/springboot2/springboot2jpacrudexample/validator/EmailValidator.java
@@ -2,18 +2,26 @@ package net.guides.springboot2.springboot2jpacrudexample.validator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.util.Arrays;
-import java.util.List;
 
 public class EmailValidator implements ConstraintValidator<EmailVal, String> {
 
-
-    List<String> emails = Arrays.asList("John.papa@gmail.com", "rambo.lars@gmail.com", "bamboo.tree@gmail.com", "abc@gmail.com");
+    String extensiveEmailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,7}$";
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
+    public boolean isValid(String email, ConstraintValidatorContext context) {
 
-        return emails.contains(value);
+        // TODO Auto-generated method stub
+        if(email == null) {
+            return false;
+        }
 
+        //Validate phone numbers of format "1234567890"
+        if (email.matches("\\^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")) return true;
+        else if(email.matches("\\^(.+)@(.+)$")) return true;
+        else if(email.matches("\\^[A-Za-z0-9+_.-]+@(.+)$")) return true;
+        else return email.matches(extensiveEmailRegex);
     }
 }
