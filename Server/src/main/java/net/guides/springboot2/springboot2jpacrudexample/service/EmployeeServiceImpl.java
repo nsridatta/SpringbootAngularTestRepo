0705476f90cc361377d5bdcbee6fade6bb471e63
nsridatta <nsridatta@gmail.com>
2021-07-28 21:35:57 +0530

    Added Caching and modified service impl

--- Server/src/main/java/net/guides/springboot2/springboot2jpacrudexample/service/EmployeeServiceImpl.java
+++ Server/src/main/java/net/guides/springboot2/springboot2jpacrudexample/service/EmployeeServiceImpl.java
@@ -4,26 +4,31 @@ import net.guides.springboot2.springboot2jpacrudexample.exception.ResourceNotFou
 import net.guides.springboot2.springboot2jpacrudexample.model.Employee;
 import net.guides.springboot2.springboot2jpacrudexample.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional
 public class EmployeeServiceImpl implements EmployeeService{
+
     @Autowired
     private EmployeeRepository employeeRepository;
-
-
 
     @Override
     public Employee createEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    @CachePut("updateemp")
     @Override
     public Employee updateEmployee(Employee employee) throws ResourceNotFoundException{
         Optional < Employee > updatedEmp = this.employeeRepository.findById(employee.getId());
@@ -31,9 +36,10 @@ public class EmployeeServiceImpl implements EmployeeService{
         if (updatedEmp.isPresent()) {
             Employee employeeUpdate = updatedEmp.get();
 
-            employeeUpdate.setFirstName(employee.getFirstName());
+            employeeUpdate.setName(employee.getName());
             employeeUpdate.setLastName(employee.getLastName());
             employeeUpdate.setEmailId(employee.getEmailId());
+            employeeUpdate.setDateOfJoining(employee.getDateOfJoining());
 
             employeeRepository.save(employeeUpdate);
             return employeeUpdate;
@@ -42,10 +48,12 @@ public class EmployeeServiceImpl implements EmployeeService{
         }
     }
 
+    @Cacheable("employees")
     @Override
     public List<Employee> getAllEmployees() {
         return this.employeeRepository.findAll();
     }
+
 
     @Override
     public Employee getEmployeeById(Long employeeId) throws ResourceNotFoundException {
@@ -57,6 +65,22 @@ public class EmployeeServiceImpl implements EmployeeService{
         }
     }
 
+    @Override
+    public Page<Employee> findAllEmployeePages(Pageable pageable) {
+        pageable = PageRequest.of(0, 10);
+        int totalPages = pageable.getPageSize();
+        return employeeRepository.findAll(pageable);
+    }
+
+    @Cacheable("employeenames")
+    @Override
+    public List<Employee> getEmployeeByName(String name) throws ResourceNotFoundException {
+
+        return this.employeeRepository.findByName(name);
+
+    }
+
+    @CacheEvict("deleteemp")
     @Override
     public void deleteEmployeeById(Long employeeId) throws ResourceNotFoundException {
         Employee employee = this.employeeRepository.findById(employeeId)
